ab88fb5fa3d8fefabe61303c466eaf02a9441861
an1kelesh <156405116+an1kelesh@users.noreply.github.com>
2024-05-20 12:36:59 +0300

    Update k8svulndb.go

--- pkg/vulnsrc/k8svulndb/k8svulndb.go
+++ pkg/vulnsrc/k8svulndb/k8svulndb.go
@@ -3,9 +3,9 @@ package k8svulndb
 import (
 	"path/filepath"
 
-	"github.com/aquasecurity/trivy-db/pkg/types"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/osv"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/vulnerability"
+	"github.com/an1kelesh/trivy-db/pkg/types"
+	"github.com/an1kelesh/trivy-db/pkg/vulnsrc/osv"
+	"github.com/an1kelesh/trivy-db/pkg/vulnsrc/vulnerability"
 )
 
 var (
