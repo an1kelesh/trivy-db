e13cc1292119c43d5c4cd8b9c2f2672e68f3c66a
an1kelesh <156405116+an1kelesh@users.noreply.github.com>
2024-05-20 12:24:00 +0300

    Update bitnami_test.go

--- pkg/vulnsrc/bitnami/bitnami_test.go
+++ pkg/vulnsrc/bitnami/bitnami_test.go
@@ -4,11 +4,11 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/aquasecurity/trivy-db/pkg/types"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/bitnami"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/bucket"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrc/vulnerability"
-	"github.com/aquasecurity/trivy-db/pkg/vulnsrctest"
+	"github.com/an1kelesh/trivy-db/pkg/types"
+	"github.com/an1kelesh/trivy-db/pkg/vulnsrc/bitnami"
+	"github.com/an1kelesh/trivy-db/pkg/vulnsrc/bucket"
+	"github.com/an1kelesh/trivy-db/pkg/vulnsrc/vulnerability"
+	"github.com/an1kelesh/trivy-db/pkg/vulnsrctest"
 )
 
 var bucketName = bucket.Name(vulnerability.Bitnami, "Bitnami Vulnerability Database")
